075e50d79f941071f16ca9b030c17ca03d16e1cb
Dániel Bordás <bordas.daniel0124@gmail.com>
2025-03-17 08:09:50 +0100

    + Beolvas metódus.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,34 @@ namespace Orai0317
     /// </summary>
     public partial class MainWindow : Window
     {
+        static List<Pet> petsLista = new();
         public MainWindow()
         {
             InitializeComponent();
+
+            string allomany = "Pets.txt";
+            Beolvas(allomany);
+        }
+
+        private void Beolvas(string allomany)
+        {
+            try
+            {
+                using (StreamReader olvas = new(allomany))
+                {
+                    olvas.ReadLine();
+
+                    while (!olvas.EndOfStream)
+                    {
+                        petsLista.Add(new Pet(olvas.ReadLine()));
+                    }
+                    MessageBox.Show("Sikeres beolvasás!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Sikertelen beolvasás " + ex.Message);
+            }
         }
     }
 }
